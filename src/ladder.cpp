d510adfe76b3765281c8afd09d6097e35250514f
Rory Lindstrom <rorylindstrom78@gmail.com>
2025-03-16 23:09:58 +0000

    Wrote generate word ladder from pseudo code

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,6 +1,7 @@
 #include "ladder.h"
 #include <cstdlib>
 
+
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
     int dif = 0;
     if (str1.length() == str2.length()) {
@@ -46,3 +47,34 @@ void load_words(set<string> & word_list, const string& file_name) {
     }
 }
 
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+
+    set<string> visited;
+    visited.insert(begin_word);
+
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+
+        string last_word = ladder.back();
+
+        for (const string & word : word_list) {
+            if (is_adjacent(last_word, word)) {
+                if (visited.find(word) == visited.end()) {
+                    visited.insert(word);
+
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+
+                    if (word == end_word) return new_ladder;
+
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+
+    return {};
+}
