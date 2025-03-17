00591d1aa600baacd1452377e930504db4a0b44d
Rory Lindstrom <rorylindstrom78@gmail.com>
2025-03-17 06:22:03 +0000

    done

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,5 +1,10 @@
 #include "dijkstras.h"
 
 int main() {
-    return 0;
+    Graph g;
+    file_to_graph("src/small.txt", g);
+    vector<int> previous;
+    vector<int> path;
+    extract_shortest_path(dijkstra_shortest_path(g, 0, previous), previous, 3);
+    print_path(path, 1);
 }
